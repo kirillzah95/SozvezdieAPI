a0f9989e57505644fe7d86616ab5566b6d0cbb9f
kirillzah95 <74539332+kirillzah95@users.noreply.github.com>
2020-11-19 18:29:55 +0300

    Правки api

--- SozvezdieAPI/Models/Offer.cs
+++ SozvezdieAPI/Models/Offer.cs
@@ -4,7 +4,7 @@ namespace SozvezdieAPI.Models
 {
     public class Offer
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string PeriodStart { get; set; }

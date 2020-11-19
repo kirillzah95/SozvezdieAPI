5bd950bd65fbbd83098c9092013c2f6256f86552
kirillzah95 <74539332+kirillzah95@users.noreply.github.com>
2020-11-19 18:30:32 +0300

    Правки api

--- SozvezdieAPI/Models/OfferData.cs
+++ SozvezdieAPI/Models/OfferData.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace SozvezdieAPI.Models
 {
-    public class OfferData
+    public class OfferData : Offer
     {
-        public int Id { get; set; }
         public string Header { get; set; }
         public List<string> Route { get; set; }
-        public string Description { get; set; }
-        public string PeriodStart { get; set; }
-        public string PeriodEnd { get; set; }
-        public string MinPrice { get; set; }
         public List<Dictionary<string, string>> PhotoAlbum { get; set; }
     }
 }
